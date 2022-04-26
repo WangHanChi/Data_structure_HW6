a34412807429914bb46514ccbd89762f18f6b6e9
WangHanChi <97653954+WangHanChi@users.noreply.github.com>
2022-04-27 07:16:00 +0800

    Update rgb.cpp

--- rgb.cpp
+++ rgb.cpp
@@ -1,54 +1,49 @@
 
-
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <fstream>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
+
 using namespace std;
 
 int diff;
 
 struct rgb {
-    string color;
-    vector<int> rgb_value;
+	string color;
+	vector<int> rgb_value;
 };
 
-struct input{
+struct input {
 	vector<int> number;
 };
 
 
-struct count{
+struct count {
 	int x;
-	count(int a ) : x(a) {}
-	bool operator()(const rgb& foo, const input& yu) const{
+	count(int a) : x(a) {}
+	bool operator()(const rgb& foo, const input& yu) const {
 		diff = 0;
-		for(int i = 0; i < foo.rgb_value.size(); ++i){
-			diff = ( abs(foo.rgb_value[i] - yu.number[i]) );
+		for (int i = 0; i < foo.rgb_value.size(); ++i) {
+			diff += (abs(foo.rgb_value[i] - yu.number[i]));
 		}
-		
-		if(diff >= x) {
+
+		if (diff >= 20) {
 			return false;
-		}else if(diff < x){
-			return true;			
+		}
+		else if (diff < 20) {
+			return true;
 		}
 	}
 };
 
 
-template< typename T, typename F>
-void print( T a, T b, F fn){
-	for( T c = a ; c != b ; ++c){
-		if( fn(*c) ){
-			cout << *c << endl;		
-		}
-	}
-}
 
-istream& operator>> (istream& in, rgb& foo){
-	while( foo.rgb_value.size()){
+
+istream& operator>> (istream& in, rgb& foo) {
+	while (foo.rgb_value.size()) {
 		foo.rgb_value.pop_back();
 	}
 	int Value;
@@ -57,73 +52,86 @@ istream& operator>> (istream& in, rgb& foo){
 	getline(in, word);
 	istringstream str(word);
 	str >> foo.color;
-	while(str >> Value){
+	while (str >> Value) {
 		foo.rgb_value.push_back(Value);
 	}
 
 	return in;
 }
 
+istream& operator>> (istream& in, input& yu) {
+	while (yu.number.size()) {
+		yu.number.pop_back();
+	}
+	int Value;
+	
+	while (in >> Value) {
+		yu.number.push_back(Value);
+	}
 
-ostream& operator<< (ostream& out, const rgb& foo){
+	return in;
+}
+
+
+ostream& operator<< (ostream& out, const rgb& foo) {
 	out << foo.color << "";
 	int sum = 0;
-	for(int i = 0; i < foo.rgb_value.size(); ++i){
+	for (int i = 0; i < foo.rgb_value.size(); ++i) {
 		sum += foo.rgb_value[i];
 		out << setw(4) << foo.rgb_value[i];
 	}
 
 	out << "";
+	return out;
 }
 
 
 int main()
 {
-    
-    ifstream infile("rgb.txt");
 
-    vector<rgb> foo;
-    rgb name;
+	ifstream infile("rgb.txt");
 
-	while(infile >> name){
-		foo.push_back( name ); 
+	vector<rgb> foo;
+	rgb name;
+
+	while (infile >> name) {
+		foo.push_back(name);
 	}
 
 
-	int i ;
-	
+	int i;
+
 	cout << "this is origin : " << endl;
-	
-	for(i = 0; i < foo.size(); ++i){
+
+	for (i = 0; i < foo.size(); ++i) {
 		cout << foo[i] << endl;
-	} 
+	}
 
 
-    do {
+	do {
 		string line;
-    	istringstream istr;
+		istringstream istr;
 		vector<input> yu;
 		input input_number;
-		
-        cout << "> ";
-        getline(cin, line);
-        istr.str(line);
-		while(istr >> input_number){
-			yu.push_back( input_number );
+
+		cout << "> ";
+		getline(cin, line);
+		istr.str(line);
+		while (istr >> input_number) {
+			yu.push_back(input_number);
 		}
 		istr.clear();
-		print(foo.begin(), foo.end(), count(20));
-		
+		//print(foo.begin(), foo.end(), count(20));
 
 
-//		for(i = 0; i < yu.size(); ++i){
-//			cout << yu[i] << " ";
-//		}
-//		cout << endl;
-    } while (1);
+
+		//		for(i = 0; i < yu.size(); ++i){
+		//			cout << yu[i] << " ";
+		//		}
+		//		cout << endl;
+	} while (1);
 
 
 
 	return 0;
 }
-
